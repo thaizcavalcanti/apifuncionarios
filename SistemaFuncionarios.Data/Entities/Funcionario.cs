ce1b59393a48a49f52ef933811f66a261d80498c
thaizcavalcanti <thaizcavalcanti@hotmail.com>
2022-08-25 20:54:18 -0300

    first commit

---
+++ SistemaFuncionarios.Data/Entities/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFuncionarios.Data.Entities
+{
+    /// <summary>
+    /// Classe de entidade
+    /// </summary>
+    public class Funcionario
+    {
+        public Guid IdFuncionario { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Matricula { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataUltimaAlteracao { get; set; }
+    }
+}
+
+
+
